ca2d17d2f0180a7e848cf692c06cca1d9349c6b5
Adiel Halevi <49694400+adiel098@users.noreply.github.com>
2025-02-17 14:23:51 +0200

    asas

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -6,7 +6,7 @@ import type { Database } from './types';
 const SUPABASE_URL = "https://trkiniaqliiwdkrvvuky.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRya2luaWFxbGlpd2RrcnZ2dWt5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk3MDUzMTMsImV4cCI6MjA1NTI4MTMxM30.3BYeVu2QCUT7LozpFWCgcP9zSfLA7v3FzO_6n6nZzM0";
 
-// Import the supabase client like this:
+// Import the supabase client like this:ד
 // import { supabase } from "@/integrations/supabase/client";
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
